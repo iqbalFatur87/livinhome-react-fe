49278d2fee55dd1680b3fc0978ce71e40469a711
iqbalFatur87 <iqbalfatur18@gmail.com>
2024-09-26 22:30:49 +0700

    some changes on localStorage

--- src/utils/constant/localStorage.ts
+++ src/utils/constant/localStorage.ts
@@ -1,14 +1,21 @@
-export const TOKEN: string = import.meta.env.VITE_TOKEN;
-export const REGISTER_TOKEN: string = import.meta.env.VITE_REGISTER_TOKEN;
-export const REGISTER_UPLOAD: string = import.meta.env.VITE_REGISTER_UPLOAD;
-export const EMAIL_VERIFICATION: string = import.meta.env.VITE_EMAIL_VERIFICATION;
-export const PHONE_VERIVICATION: string = import.meta.env.VITE_PHONE_VERIVICATION;
-export const OTP_VERIFICATION: string = import.meta.env.VITE_OTP_VERIFICATION;
-export const SUCCESS_RESET_PASSWORD: string = import.meta.env.VITE_SUCCESS_RESET_PASSWORD;
-export const SUCCESS_REGISTER: string = import.meta.env.VITE_SUCCESS_REGISTER;
+export const TOKEN: string = "TOKEN";
+export const REGISTER_TOKEN: string = "REGISTER_TOKEN";
+export const REGISTER_UPLOAD: string = "REGISTER_UPLOAD";
+export const EMAIL_VERIFICATION: string = "EMAIL_VERIFICATION";
+export const PHONE_VERIVICATION: string = "PHONE_VERIVICATION";
+export const OTP_VERIFICATION: string = "OTP_VERIFICATION";
+export const SUCCESS_RESET_PASSWORD: string = "SUCCESS_RESET_PASSWORD";
+export const SUCCESS_REGISTER: string = "SUCCESS_REGISTER";
+export const ROLE: string = "ROLE";
+export const SECRET_KEY: string = import.meta.env.VITE_SECRET_KEY;
+export const DATA: string = import.meta.env.VITE_LOCAL_STORAGE;
 
-export const AUTHORIZATION_HEADERS = {
-  headers: {
-    Authorization: `${localStorage.getItem(TOKEN)}`,
-  },
-};
+// export const TOKEN: string = import.meta.env.VITE_TOKEN;
+// export const REGISTER_TOKEN: string = import.meta.env.VITE_REGISTER_TOKEN;
+// export const REGISTER_UPLOAD: string = import.meta.env.VITE_REGISTER_UPLOAD;
+// export const EMAIL_VERIFICATION: string = import.meta.env.VITE_EMAIL_VERIFICATION;
+// export const PHONE_VERIVICATION: string = import.meta.env.VITE_PHONE_VERIVICATION;
+// export const OTP_VERIFICATION: string = import.meta.env.VITE_OTP_VERIFICATION;
+// export const SUCCESS_RESET_PASSWORD: string = import.meta.env.VITE_SUCCESS_RESET_PASSWORD;
+// export const SUCCESS_REGISTER: string = import.meta.env.VITE_SUCCESS_REGISTER;
+// export const ROLE: string = import.meta.env.VITE_ROLE;
