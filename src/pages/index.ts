5d5889b1fefe90fadf51791dc4e8f4103354cf20
iqbalFatur87 <iqbalfatur18@gmail.com>
2024-10-02 09:11:29 +0700

    changes for pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -6,12 +6,20 @@ export { default as ResetPassword } from "./Authentication/ResetPassword";
 export { default as Register } from "./Authentication/Register";
 
 // OWNER
-export { default as DaftarProperti } from "./Owner/DaftarProperti";
-export { default as ManagementProperti } from "./Owner/ManagemenProperti";
-export { default as PengajuanSewa } from "./Owner/PengajuanSewa";
 export { default as DashboardOwner } from "./Owner/Dashboard";
-export { default as PengajuanSurvei } from "./Owner/PengajuanSurvei";
-export { default as ListPengajuanSewa } from "./Owner/PengajuanSewa/List";
-export { default as KonfirmasiPenyewa } from "./Owner/PengajuanSewa/Confirm";
-export { default as InfoCalonPenyewaSewa } from "./Owner/PengajuanSewa/Info";
+
+export { default as DaftarProperti } from "./Owner/DaftarProperti";
+
+export { default as ManagementProperti } from "./Owner/ManagemenProperti";
 export { default as DetailProperti } from "./Owner/ManagemenProperti/Detail";
+
+export { default as ListPengajuanSewa } from "./Owner/PengajuanSewa/List";
+export { default as InfoCalonPenyewaSewa } from "./Owner/PengajuanSewa/Info";
+export { default as KonfirmasiPenyewa } from "./Owner/PengajuanSewa/Confirm";
+
+export { default as PengajuanSurvei } from "./Owner/PengajuanSurvei";
+export { default as Profil } from "./Owner/Profil";
+
+//Layout
+export { default as LayoutOwner } from "./Owner/Layout";
+export { default as LayoutPengajuanSewa } from "./Owner/PengajuanSewa/Layout";
