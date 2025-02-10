92623a221f4407b4d45c9a68749751da03a376cf
iqbalFatur87 <iqbalfatur18@gmail.com>
2025-02-10 22:45:25 +0700

    removed unused import

--- src/pages/Admin/list-penyewa/detail/index.tsx
+++ src/pages/Admin/list-penyewa/detail/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Input, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Flex, Input, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
